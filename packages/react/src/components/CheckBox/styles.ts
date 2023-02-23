d9411e89d2ac4c3bdde0b2e1c47c5d1715059276
MJB2012 <matheusjbarbosa23@gmail.com>
2023-02-23 18:20:24 -0300

    Checkbox Component

--- packages/react/src/components/CheckBox/styles.ts
+++
@@ -1,6 +0,0 @@
-import * as Checkbox from '@radix-ui/react-checkbox'
-import { styled } from '../../styles'
-
-export const CheckboxContainer = styled(Checkbox.Root, {})
-
-export const CheckboxIndicator = styled(Checkbox.Indicator, {})
